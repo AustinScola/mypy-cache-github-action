8dff3625c5e400d78325b14aeb1653c6bd5e9e5c
Austin Scola <austinscola@gmail.com>
2021-06-15 17:31:27 -0400

    Add save cache functionality
    Add basic save cache functionality

--- tests/save.test.ts
+++ tests/save.test.ts
@@ -1,5 +1,23 @@
-import {save} from "../src/save";
+import * as cache from "@actions/cache";
 
-test("test save", () => {
-  save();
+test("saves the mypy cache", async () => {
+  const cacheIdMock = 12345;
+  jest.spyOn(cache, "saveCache").mockResolvedValue(cacheIdMock);
+
+  const mockSHA = "086ffe45156e33a337859ff77cd720cdc1e9cb40";
+  jest.doMock("@actions/github", () => ({
+    __esModule: true,
+    context: {sha: mockSHA},
+  }));
+
+  const save = require("../src/save").save;
+
+  await save();
+
+  expect(cache.saveCache).toHaveBeenCalledTimes(1);
+
+  const expectedPaths = [".mypy_cache"];
+  const expectedKey = "mypy-cache-086ffe45156e33a337859ff77cd720cdc1e9cb40";
+  const expectedSaveCacheArgs = [expectedPaths, expectedKey];
+  expect(cache.saveCache).toHaveBeenCalledWith(...expectedSaveCacheArgs);
 });
